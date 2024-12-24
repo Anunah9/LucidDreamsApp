f742df17f95aa94f5e4ad8e2db740beabc25b568
Aleksandr <sanek00000007@gmail.com>
2024-12-24 15:32:15 +0300

    -Added heart rate measurment -Added accelerometer measurment

--- DeviceApp/page/index.js
+++ DeviceApp/page/index.js
@@ -1,9 +1,8 @@
-import { log as Logger } from "@zos/utils";
 import { BasePage } from "@zeppos/zml/base-page";
 import { createWidget, widget } from "@zos/ui";
 import { HeartRate } from '@zos/sensor'
 import { setWakeUpRelaunch } from '@zos/display'
-
+import { Accelerometer, FREQ_MODE_NORMAL, FREQ_MODE_LOW } from '@zos/sensor'
 
 
 Page(
@@ -16,27 +15,53 @@ Page(
       setWakeUpRelaunch({
         relaunch: true,
       })
-      let healthData = {}
+
+
+      const accelerometer = new Accelerometer()
+      const callback = () => {
+        coord = accelerometer.getCurrent()
+        console.log("Current coord", coord.x, coord.y, coord.z)
+      }
+      accelerometer.onChange(callback)
+      accelerometer.setFreqMode(FREQ_MODE_NORMAL)
+
+
       const heartRate = new HeartRate()
       const curCallback = () => {
         console.log("Start measure")
         console.log(heartRate.getCurrent())
       }
-      let params = {
+
+
+      let params_start_btn = {
         x: 170,
-        y: 200,
+        y: 150,
         text: 'Start',
         w: -1,
         h: -1,
-
         click_func: () => {
           console.log('button click')
           heartRate.onCurrentChange(curCallback)
-
+          accelerometer.start()
         }
       }
 
-      createWidget(widget.BUTTON, params)
+      createWidget(widget.BUTTON, params_start_btn)
+
+      let params_stop_btn = {
+        x: 170,
+        y: 290,
+        text: 'Stop',
+        w: -1,
+        h: -1,
+        click_func: () => {
+          console.log('Stop clicked')
+          heartRate.offCurrentChange(curCallback)
+          accelerometer.stop()
+        }
+      }
+
+      createWidget(widget.BUTTON, params_stop_btn)
     },
     onRequest(req, res) {
 
@@ -44,7 +69,6 @@ Page(
         console.log("OnRequest, GET_HEALTH_DATA")
         let healthData = this.getHealthData()
         res(null, healthData)
-
       } else {
         res('error happened')
       }
