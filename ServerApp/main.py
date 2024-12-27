c7e66d94c639dbc2dcb7fdf589ce6e5d1114cdcd
Aleksandr <sanek00000007@gmail.com>
2024-12-27 10:24:46 +0300

    -Added data to the database

--- ServerApp/main.py
+++ ServerApp/main.py
@@ -1,5 +1,7 @@
+import datetime
 import json
 from pprint import pprint
+import sqlite3
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -9,22 +11,59 @@ import uvicorn
 app = FastAPI()
 
 
+def to_db():
+    pass
+
+
+def heartrate_handler(data):
+    with sqlite3.connect("./db.db") as db:
+        cursor = db.cursor()
+
+        data = (data["time"], data["hr"])
+        # print(data)
+        query = "INSERT INTO heartrate VALUES (?, ?)"
+        cursor.execute(query, data)
+
+        db.commit()
+
+
+def position_handler(name, data_raw):
+    with sqlite3.connect("./db.db") as db:
+        cursor = db.cursor()
+        data = []
+        for i in data_raw:
+            data.append(tuple(i[1] for i in list(i.items())))
+        # print(data)
+        if name == "axel":
+            query = "INSERT INTO accelerometer VALUES (?, ?, ?, ?)"
+            cursor.executemany(query, data)
+        elif name == "guro":
+            query = "INSERT INTO gyroscope VALUES (?, ?, ?, ?)"
+            cursor.executemany(query, data)
+        db.commit()
+
+
 class Health(BaseModel):
     health: str
 
+
 def sort_data(data):
-    if '0' in data.keys():
-        name = data['0']['name']
-        data_raw = [data[i]['data'] for i in data.keys()]
-        pprint(name)
-        pprint(data_raw)
-    elif 'name' in data.keys():
+    if "0" in data.keys():
+        name = data["0"]["name"]
+        data_raw = [data[i]["data"] for i in data.keys()]
+        # pprint(name)
+        position_handler(name, data_raw)
+        # pprint(data_raw)
+    elif "name" in data.keys():
         name = "hr"
-        data = data['data']
+        data = data["data"]
+        heartrate_handler(data)
         pprint(data)
+
 
 def to_db(data):
     pass
+
 
 @app.get("/")
 def read_root():
@@ -39,9 +78,11 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.post("/health")
 def read_health_data(health: Health):
     data = json.loads(health.health)
+    print(datetime.time)
     sort_data(data)
     # pprint(data)
 
 
 if __name__ == "__main__":
+
     uvicorn.run(app, host="0.0.0.0", port=5000)
